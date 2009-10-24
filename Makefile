820cba0c06c972d82951ef77633318f688c74b4b
Damien Lebrun <dinoboff@hotmail.com>
2009-10-24 04:06:21 +0100

    Add Makefile.

---
+++ Makefile
@@ -0,0 +1,40 @@
+SRC_DIR = .
+BUILD_DIR = build
+PREFIX = .
+DIST_DIR = ${PREFIX}/dist/
+
+SRC_FILES = ${SRC_DIR}/jqswipe.js
+
+RELEASE_TYPE = -dev
+VERSION = `cat version.txt`${RELEASE_TYPE}
+LAST_COMMIT=`git log -1 | grep Date: | sed 's/[^:]*: *//'`
+
+JQSWIPE = ${DIST_DIR}jqswipe.js
+JQSWIPE_MIN = ${DIST_DIR}jqswipe.min.js
+
+
+MINJAR = java -jar ${BUILD_DIR}/yuicompressor-2.4.2.jar
+VER = sed s/@VERSION/${VERSION}/
+DATE = sed 's/@DATE/'"${LAST_COMMIT}"'/'
+all: jqswipe min
+
+jqswipe: ${JQSWIPE}
+
+min: ${JQSWIPE_MIN}
+
+${JQSWIPE}: ${SRC_FILES} ${DIST_DIR}
+	@@echo "Building ${JQSWIPE} ..."
+	@@cat ${SRC_FILES} | ${VER} | ${DATE} > ${JQSWIPE}
+	@@echo "built!"
+
+${JQSWIPE_MIN}: ${JQSWIPE}
+	@@echo "Compressing ${JQSWIPE} ..."
+	@@${MINJAR} ${JQSWIPE} > ${JQSWIPE_MIN}
+	@@echo ${JQSWIPE_MIN} built!
+
+${DIST_DIR}:
+	@@mkdir -p ${DIST_DIR}
+
+clean:
+	@@rm -rf ${DIST_DIR}
+	@@echo "cleaned"
--- jqswipe.js
+++ jqswipe.js
@@ -1,38 +1,21 @@
-/**
- * jQswipe 0.0.1 - Swipe event for jQuery.
- *
- * based on "Safari Web Content Guide > Handling Events > Handling Multi-Touch Events"
- * http://bit.ly/Q6uOD
- *
+/*!
+ * jQswipe v@VERSION - Swipe event for jQuery.
  * http://github.com/dinoboff/jQswipe/
  *
  * Copyright (c) 2009 Damien Lebrun
- * All rights reserved.
- * 
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
- * 
- *	   * Redistributions of source code must retain the above copyright notice,
- *		 this list of conditions and the following disclaimer.
- * 
- *	   * Redistributions in binary form must reproduce the above copyright notice,
- *		 this list of conditions and the following disclaimer in the documentation
- *		 and/or other materials provided with the distribution.
- * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
+ * licenced under BSD
+ * http://github.com/dinoboff/jQswipe/blob/master/LICENSE
+ * Date: @DATE
  */
  
- 
+/**
+ * based on "Safari Web Content Guide > Handling Events > Handling Multi-Touch Events"
+ * http://bit.ly/Q6uOD
+ * 
+ * Implement special events as explained by Brandon Aaron:
+ * http://brandonaaron.net/blog/2009/03/26/special-events
+ */
+
  (function() {
 	var window = this,
 	$ = window.jQuery;
@@ -93,10 +76,7 @@
 	
 	
 	/**
-	 * Swipe object
-	 * 
-	 * Used to to create special events:
-	 * http://brandonaaron.net/blog/2009/03/26/special-events
+	 * Swipe object - special event factory
 	 */
 	 
 	/**
