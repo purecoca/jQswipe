3392bab1667326877eaa0a1850e9aea2906424d2
Damien Lebrun <dinoboff@hotmail.com>
2009-10-29 00:25:49 +0000

    update jqswipe

--- jqswipe.js
+++ jqswipe.js
@@ -18,306 +18,302 @@
  */
 
  (function() {
-	var jQswipe,
-		window = this,
-		$ = window.jQuery;
+	var window = this,
+		$ = window.jQuery,
+		jQswipe = $.jQswipe = {},
+		STATE;
 	
 	/**
 	 * Monkey patch to add the touches and targetTouches properties
 	 * to jQuery.Event objects.
 	 */
-	 $.each(['touches', 'targetTouches'], function(){
-		 if (!($.inArray($.event.props, this))) {
-			 $.event.props.push(this);
-		 }
-	 });
-	
-	// jQswipe namespace
-	jQswipe = $.jQswipe = {};
-	
+	$.each(['touches', 'targetTouches'], function(i, propName){
+		if ( $.inArray(propName, $.event.props) < 0 ) {
+			$.event.props.push(propName);
+		}
+	});
 	
 	/**
-	 * Point - representation of a point on the page
-	 * 
-	 * Used to store touch history 
+	 * Collection of touches - used to record finger position of a swipe
 	 */
-	
-	/**
-	 * Constructor
-	 */
-	jQswipe.Point = function(x, y) {
-		this.x = x;
-		this.y = y;
+	jQswipe.Swipe = function(settings) {
+		$.extend(this, settings);
 	};
 	
-	
-	/**
-	 * return the difference on x and y between two point.
-	 */
-	jQswipe.Point.prototype.diff = function(point) {
-		return {
-			x: this.x - point.x,
-			y: this.y - point.y
-		};
+	STATE = jQswipe.Swipe.STATE = {
+		RUNNING: 1,
+		ENDED: 2,
+		CANCELLED: -1
 	};
 	
-	/**
-	 * String representation of a Point.
-	 */
-	jQswipe.Point.prototype.toString = function() {
-		return 'x: ' + this.x + ', y: ' + this.y;
+	jQswipe.Swipe.prototype = {
+		touches: [],
+		type: 'swipe',
+		state: null,    // State of the swipe, should be set ot one the jQswipe.Swipe.STATE properties
+		minLength: 30,  // How long the swipe should be at least.
+		maxWidth: 10,   // How width it can be up to.
+		keepLast: 2,    // How many touches should keeped after each upgrad (0 to keep all).
+		
+		reset: function(touch) {
+			this.touches = [];
+			this.state = STATE.RUNNING;
+		},
+		
+		/**
+		 * Add a touch to the swipe.
+		 * 
+		 * return the index of the touch (after compression).
+		 * 
+		 * `touches` should be an array of touch (like window.event.targetTouches).
+		 * If there are too many or no touches, the state of the swipe is set to
+		 * STATE.CANCELLED and -1 is returned.
+		 */
+		push: function(touches) {
+			var touch;
+			
+			if (touches.length === 1) {
+				touch = touches[0];
+			}
+
+			if (touch && touch.pageX !== undefined && touch.pageY !== undefined) {
+				this.touches.push(touch);
+				this.compress();
+				return this.size() -1;
+			}
+			
+			this.cancelled();
+			return -1;
+		},
+		
+		/**
+		 * return the touches length
+		 */
+		size: function(touches) {
+			return this.touches.length;
+		},
+		
+		/**
+		 * Remove old touches.
+		 * 
+		 * should remove all points except start, previous and current point.
+		 */
+		compress: function() {
+			var length = this.size(),
+				cleanUpLength;
+				
+			if (!this.keepLast || this.keepLast < 1) {
+				return;
+			}
+			
+			cleanUpLength = length - this.keepLast - 1;
+			if (cleanUpLength > 0) {
+				this.touches.splice(1, cleanUpLength);
+			}
+		},
+		
+		/**
+		 * Return the touch at the giving index.
+		 *
+		 * Accept negative index (index from the end of the swipe);
+		 * e.g., -1 is the last touch of the swipe.
+		 */
+		atIndex: function(index) {
+			var length = this.touches.length;
+			
+			if (length < 1) {
+				return;
+			}
+			
+			index = ((Math.floor(Math.abs(index / length)) +1) * length + index) % length;
+			return this.touches[index];
+		},
+		
+		/**
+		 * Return the difference on x an y between two touches.
+		 */ 
+		diff: function(startIndex, endIndex) {
+			var startPoint = this.atIndex(startIndex),
+				endPoint = this.atIndex(endIndex);
+			
+			return {
+				x: endPoint.pageX - startPoint.pageX,
+				y: endPoint.pageY - startPoint.pageY
+			};
+		},
+		
+		/**
+		 * Validate the swipe. 
+		 */
+		validate: function() {
+			var diffWithStart = this.diff(0, -1),
+				diffWithPrevious = this.diff(-2, -1);
+			
+			// If the swipe has been cancelled in the past,
+			// it cannot be valide any more.
+			if (this.state === STATE.CANCELLED) {
+				return false;
+			}
+			
+			// validate progression
+			if (diffWithStart.y > this.maxWidth ||  // Should not be too hight
+				diffWithStart.y < -this.maxWidth || // or too low
+				diffWithPrevious.x < 0              // should not move back to the left
+			) {
+				this.cancel();
+				return false;
+			}
+			
+			// Validate the comple swipe
+			if (this.state === STATE.ENDED && diffWithStart.x < this.minLength) {
+				return false;
+			}
+			
+			return true;
+		},
+		
+		cancel: function() {
+			this.state = STATE.CANCELLED;
+		},
+		
+		end: function() {
+			this.state = STATE.ENDED;
+		}
 	};
 	
+		
 	
 	/**
-	 * Create a point from a a pair of pageX/pageY.
-	 */
-	jQswipe.Point.fromTouch = function(touch) {
-		return new $.jQswipe.Point(touch.pageX, touch.pageY);
-	};
-	
-	
-	/**
-	 * Swipe object - special event factory
+	 * SwipeSpecialEvent object - special event factory
 	 */
 	 
 	/**
 	 * Constructor
-	 * 
-	 * Set the setup and teardown methodes. They bind safari touch events to store
-	 * touches position and validate them.
-	 *
-	 * The default validator make sure a swipe always go to the right,
-	 * and doesn't go up or down (not more than 10 pixel by default).
-	 *
-	 * Also set the swipe data namespace for storing the touches.  
 	 */
-	jQswipe.Swipe = function(eventType, validator) {
+	jQswipe.SwipeSpecialEvent = function(eventType, settings) {
 		var that = this;
-	
-		eventType = eventType || 'swipe';
+
+		this.settings = settings || {};
+		this.settings.type = eventType || 'swipe';
 		
-		this.dataPrefix = 'events.special.' + eventType;
-		if (validator) {
-			this.validate = validator;
-		}
-	
 		this.setup = function(data, namespaces) {
-			var $el = $(this);
-			$el.bind('touchstart', that.startHandler);
-			$el.bind('touchmove', that.moveHandler);
-			$el.bind('touchcancel', that.cancelHandler);
-			$el.bind('touchend', that.endHandler);
+			var $elem = $(this),
+				ns = that.prepareNamespaces(namespaces),
+				swipe = new $.jQswipe.Swipe(that.settings);
+			
+			$.each(['touchstart', 'touchmove', 'touchcancel', 'touchend'], function(i, val){
+				ns[0] = val;
+				$elem.bind(ns.join('.'), swipe, that[val]);
+				
+				// Remove handler guid (set by jQuery to track handler).
+				// We don't need them, we remove event handler using namespace
+				delete that[val].guid;
+			});
 		};
-	
+		
 		this.teardown = function(namespaces) {
-			var $el = $(this);
-			$el.unbind('touchstart', that.startHandler);
-			$el.unbind('touchmove', that.moveHandler);
-			$el.unbind('touchcancel', that.cancelHandler);
-			$el.unbind('touchend', that.endHandler);
+			var $elem = $(this),
+				ns = that.prepareNamespaces(namespaces);
+			
+			$.each(['touchstart', 'touchmove', 'touchcancel', 'touchend'], function(i, val){
+				ns[0] = val;
+				$elem.unbind(ns.join('.'));
+			});
 		};
+	};
 	
-		that.startHandler = function(event) {
-			that.start(this, that.getTouches(event));
-		};
-	
-		that.moveHandler = function(event) {
-			that.update(this, that.getTouches(event));
-		};
-	
-		that.cancelHandler = function() {
-			that.cancelled(this, true);
-		};
-	
-		that.endHandler = function(event) {
-			that.end(this);
-	
-			if (that.cancelled(this) === false) {
-				event.type = eventType;
+	jQswipe.SwipeSpecialEvent.prototype = {
+		
+		prepareNamespaces: function(swipeNamespaces) {
+			var copy = swipeNamespaces.slice(0, swipeNamespaces.length);
+			
+			copy.splice(0, 0, '', this.settings.type);
+			return copy;
+		},
+		
+		/**
+		 * Reset the swipe state and add the first touch.
+		 */
+		touchstart: function(event) {
+			var swipe = event.data;
+			
+			swipe.reset();
+			swipe.push(event.targetTouches || []);
+		},
+		
+		/**
+		 * Add a new touch if the swipe is still valid
+		 */
+		touchmove: function(event) {
+			var swipe = event.data;
+			
+			if (swipe.validate()) {
+				swipe.push(event.targetTouches || []);
+			}
+		},
+		
+		/**
+		 * Cancel swipe
+		 */
+		touchcancel: function(event) {
+			var swipe = event.data;
+			
+			swipe.cancel();
+		},
+		
+		/**
+		 * End swipe and trigger the swipe event if the swipe is valid
+		 */
+		touchend: function(event) {
+			var swipe = event.data;
+			swipe.end();
+			
+			if (swipe.validate()) {
+				event.type = swipe.type;
 				$.event.handle.apply(this, arguments);
 			}
-	
-		};
-	};
-	
-	/**
-	 * prototype methodes
-	 */
-	jQswipe.Swipe.prototype = {
-		
-		/**
-		 * prefix for data name used with $.fn.data(name, value)
-		 */
-		dataPrefix: 'events.special.swipe',
-		
-		/**
-		 * limits for validating a swipe.
-		 */
-		bound: {
-			minLength: 30,
-			maxWidth: 10
-		},
-	
-		/**
-		 * Set initials value of swipe event and save the start position
-		 */ 
-		start: function(el, touches) {
-			var currentPoint;
-	
-			if (touches.length === 1) {
-				currentPoint = $.jQswipe.Point.fromTouch(touches[0]);
-				this.cancelled(el, false);
-				this.ended(el, false);
-				this.startPoint(el, currentPoint);
-				this.currentPoint(el, currentPoint);
-			} else {
-				this.cancelled(el, true);
-			}
-		},
-	
-		/**
-		 * Update the last position of finger and validate it
-		 */
-		update: function(el, touches) {
-			var newPoint;
-	
-			if (this.cancelled(el)) {
-				return;
-			}
-	
-			if (touches && touches.length === 1) {
-				this.previousPoint(el, this.currentPoint(el));
-			} else {
-				this.cancelled(el, true);
-				return;
-			}
-	
-			newPoint = $.jQswipe.Point.fromTouch(touches[0]);
-			if (this.validate(el, newPoint)) {
-				this.currentPoint(el, newPoint);
-			} else {
-				this.cancelled(el, true);
-			}
-		},
-	
-		/**
-		 * Default swipe validator. Validate a left swipe
-		 */
-		validate: function(el, newPoint) {
-			// validate swipe (a right swipe by default)
-			var diffWithStart = newPoint.diff(this.startPoint(el)),
-				diffWithPrevious = newPoint.diff(this.previousPoint(el));
-			
-			// Should not be hight or too low
-			if (diffWithStart.y > this.bound.maxWidth || diffWithStart.y < -this.bound.maxWidth) {
-				return false;
-			}
-	
-			// should not move back to the left
-			if (diffWithPrevious.x < 0) {
-				return false;
-			}
-	
-			// If swipe ended, the swipe be long enough
-			if (this.ended(el) && diffWithStart.x < this.bound.minLength) {
-				return false;
-			}
-	
-			return true;
-		},
-	
-		/**
-		 * Validate the last position of finger
-		 */
-		end: function(el) {
-			this.ended(el, true);
-			
-			// we don't need to update it if it's already true
-			if (this.cancelled(el) === false &&
-				this.validate(el, this.currentPoint(el)) === false
-			) {
-				this.cancelled(el, true);
-			}
-		},
-		
-		/**
-		 * Accessors
-		 */
-		startPoint: function(el, point) {
-			return this.data(el, 'start', point);
-		},
-	
-		previousPoint: function(el, point) {
-			return this.data(el, 'previous', point);
-		},
-	
-		currentPoint: function(el, point) {
-			return this.data(el, 'current', point);
-		},
-	
-		cancelled: function(el, cancelled) {
-			return this.data(el, 'cancelled', cancelled);
-		},
-	
-		ended: function(el, ended) {
-			return this.data(el, 'ended', ended);
-		},
-	
-		/**
-		 * Helpers
-		 */
-		 data: function(el, name, value) {
-			 var result = $(el).data(this.dataPrefix + '.' + name, value);
-			 if (value === undefined) {
-				 return result;
-			 }
-		 }, 
-		
-		getTouches: function(event) {
-			var touches = window.event && window.event.targetTouches;
-			if (touches === undefined && window.console) {
-				console.warn('No event.targetTouches');
-			}
-			return touches;
 		}
 	};
 	
+
 	/**
 	 * Register special swipe event
-	 * 
-	 * Take the event name,
-	 * and the validator function that will validate each finger position of the swipe
 	 */
-	jQswipe.register = function(type, validator) {
-		$.event.special[type] = new this.Swipe(type, validator);
+	jQswipe.register = function(type, settings) {
+		$.event.special[type] = new $.jQswipe.SwipeSpecialEvent(type, settings);
 	};
-	
+
 	/**
 	 * Register by default a right and left swipe.
 	 */
 	jQswipe.register('swipe');
 	jQswipe.register('rightSwipe');
-	jQswipe.register('leftSwipe', function(el, newPoint) {
-		var diffWithStart = newPoint.diff(this.startPoint(el)),
-			diffWithPrevious = newPoint.diff(this.previousPoint(el));
+	jQswipe.register('leftSwipe', {
+		validate: function() {
+			var diffWithStart = this.diff(0, -1),
+				diffWithPrevious = this.diff(-2, -1);
 	
-		// Should not be hight or too low
-		if (diffWithStart.y > this.bound.maxWidth || diffWithStart.y < -this.bound.maxWidth) {
-			return false;
+			// If the swipe has been cancelled in the past,
+			// it cannot be valide any more.
+			if (this.state === STATE.CANCELLED) {
+				return false;
+			}
+	
+			// validate progression
+			if (diffWithStart.y > this.maxWidth ||  // Should not be too hight
+				diffWithStart.y < -this.maxWidth || // or too low
+				diffWithPrevious.x > 0              // should not move back to the right
+			) {
+				this.cancel();
+				return false;
+			}
+	
+			// Validate the comple swipe
+			if (this.state === STATE.ENDED && diffWithStart.x > -(this.minLength)) {
+				return false;
+			}
+	
+			return true;
 		}
-	
-		// should not move back to the right
-		if (diffWithPrevious.x > 0) {
-			return false;
-		}
-	
-		// If swipe ended, the swipe be long enough
-		if (this.ended(el) && diffWithStart.x > -this.bound.minLength) {
-			return false;
-		}
-	
-		return true;
 	});
 	
 	/**
